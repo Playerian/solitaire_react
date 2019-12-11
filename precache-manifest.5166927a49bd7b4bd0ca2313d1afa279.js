24da8ac30030fc21b7c813565969abbd7ea56871
Playerian <you@example.com>
2019-12-10 17:42:30 -0800

    Updates

---
+++ precache-manifest.5166927a49bd7b4bd0ca2313d1afa279.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bad5d176ab76ee81ca871a1ff88c12ce",
+    "url": "/solitaire_react/index.html"
+  },
+  {
+    "revision": "65798b55eb16bfd96401",
+    "url": "/solitaire_react/static/css/main.04c174dc.chunk.css"
+  },
+  {
+    "revision": "d5926e857513ad030148",
+    "url": "/solitaire_react/static/js/2.174733d6.chunk.js"
+  },
+  {
+    "revision": "65798b55eb16bfd96401",
+    "url": "/solitaire_react/static/js/main.31b9b22b.chunk.js"
+  },
+  {
+    "revision": "64f45f5411ccaabb9bf1",
+    "url": "/solitaire_react/static/js/runtime-main.8c18c5af.js"
+  }
+]);
